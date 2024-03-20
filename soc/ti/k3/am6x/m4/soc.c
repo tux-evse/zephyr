b79339beadbe96596fe682caff9077b63504dad0
Andrew Davis <afd@ti.com>
2024-03-20 15:34:50 -0500

    soc: am6x: Simplify default RAT configuration
    Signed-off-by: Andrew Davis <afd@ti.com>
    Signed-off-by: Aymeric Aillet <aymeric.aillet@iot.bzh>

--- soc/ti/k3/am6x/m4/soc.c
+++ soc/ti/k3/am6x/m4/soc.c
@@ -10,35 +10,29 @@
 #include <zephyr/init.h>
 #include <soc.h>
 
-#define ADDR_TRANSLATE_RAT_BASE_ADDR		(0x044200000u)
-#define PINCTRL_BASE_ADDR			(0x4080000u)
+#define ADDR_TRANSLATE_RAT_BASE_ADDR		(0x44200000u)
+#define PINCTRL_BASE_ADDR			(0x04080000u)
 #define KICK0_UNLOCK_VAL			(0x68EF3490U)
 #define KICK1_UNLOCK_VAL			(0xD172BC5AU)
 #define CSL_MCU_PADCONFIG_LOCK0_KICK0_OFFSET	(0x1008)
 #define CSL_MCU_PADCONFIG_LOCK1_KICK0_OFFSET	(0x5008)
 
 static struct address_trans_region_config am6x_region_config[] = {
+//	{
+//		.system_addr = 0x44200000u,
+//		.local_addr =  0x44200000u,
+//		.size = address_trans_region_size_4K,
+//	},
 	{
-		.system_addr = 0x0u,
-		.local_addr = 0x80000000u,
-		.size = address_trans_region_size_512M,
+		.system_addr = 0x29000000u,
+		.local_addr =  0xb0000000u,
+		.size = address_trans_region_size_4K,
 	},
 	{
-		.local_addr = 0xA0000000u,
-		.system_addr = 0x20000000u,
-		.size = address_trans_region_size_512M,
+		.system_addr = 0x00000000u,
+		.local_addr =  0xc0000000u,
+		.size = address_trans_region_size_1G,
 	},
-	{
-		.local_addr = 0xC0000000u,
-		.system_addr = 0x40000000u,
-		.size = address_trans_region_size_512M,
-	},
-	{
-		.local_addr = 0x60000000u,
-		.system_addr = 0x60000000u,
-		.size = address_trans_region_size_512M,
-	},
-
 /*
  * Add regions here if you want to map more memory.
  */
